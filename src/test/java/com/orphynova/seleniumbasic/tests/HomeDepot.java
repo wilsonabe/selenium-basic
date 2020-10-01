28498e7c3e6bbadf127d401af0f62e8097ff3f22
wilsonabe <wilson.abe@gmail.com>
2020-10-01 18:06:15 -0400

    Test for selecting radio buttons. Closes #3

--- src/test/java/com/orphynova/seleniumbasic/tests/HomeDepot.java
+++ src/test/java/com/orphynova/seleniumbasic/tests/HomeDepot.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -14,26 +15,29 @@ public class HomeDepot {
     private final String baseURL = "https://www.homedepot.ca/en/home.html";
     private WebDriver driver;
 
-    @Test
-    public void testHomePageTitle() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
+    @BeforeTest
+    public void beforeTest(){
+         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
         driver = new ChromeDriver();
         driver.get(baseURL);
+    }
+
+    @Test
+    public void testHomePageTitle() throws InterruptedException {
 
         //meta name in source
         String expTitle = "Home Improvement, Home Renovation, Tools, & Hardware | The Home Depot Canada";
         String actTitle = driver.getTitle();
         Assert.assertEquals(expTitle,actTitle,"Incorrect Webpage Title");
 //        Thread.sleep(2000);
-
-
     }
 
     @Test
     public void testSearchingForAProduct()  {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get(baseURL);
 
         //meta title in source
         String expTitle = "Home Improvement, Home Renovation, Tools, & Hardware | The Home Depot Canada";
@@ -44,19 +48,14 @@ public class HomeDepot {
         txtSearch.clear();
         txtSearch.sendKeys("tables");
 
-        WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"gheader-autosuggest-input-submit\"]/acl-icon/svg"));
-        btnSearch.click();
-
-    }
-
-    @BeforeTest
-    public void beforeTest(){
-        //   WebDriverManager.chromedriver().setup();
+        driver.findElement(By.id("gheader-autosuggest-input-submit")).click();
+//        WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"gheader-autosuggest-input-submit\"]/acl-icon/svg"));
+//        btnSearch.click();
     }
 
     @AfterMethod
     public void CloseBrowser(){
-            driver.quit(); // instance is quit
+         //   driver.quit(); // instance is quit
 
     }
 }
