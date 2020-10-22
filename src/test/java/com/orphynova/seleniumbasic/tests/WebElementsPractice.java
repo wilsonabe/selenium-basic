5aa005aa4d42e834a8c8d1de7faeb2fde738ec2f
wilsonabe <wilson.abe@gmail.com>
2020-10-21 21:28:44 -0400

    Web element controls

--- src/test/java/com/orphynova/seleniumbasic/tests/WebElementsPractice.java
+++ src/test/java/com/orphynova/seleniumbasic/tests/WebElementsPractice.java
@@ -1,17 +1,20 @@
 package com.orphynova.seleniumbasic.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class WebElementsPractice {
     private String baseURL = "http://qaguru.ca/webelementapp.php";
@@ -48,4 +51,87 @@ public class WebElementsPractice {
     //   driver.findElement(By.xpath("//*[@id=\"home-5\"]/form[2]/input[2]")).click();
 
     }
+    @Test
+    public void setTextBox(){
+        WebElement nameTxtBox = driver.findElement(By.name("firstname"));
+        nameTxtBox.clear();
+        nameTxtBox.sendKeys("John");
+
+//        Alternatively the code can be written as
+//        driver.findElement(By.name("firstname")).clear();
+//        driver.findElement(By.name("firstname")).sendKeys("Mary");
+    }
+
+    //***************************************************************************************
+    // List Box/ Dropdown ListBox operations
+    @Test
+    public void listBox(){
+        WebElement dropListBox = driver.findElement(By.name("cars"));
+
+        // To select a value in the list box - use SELECT class
+        Select selList = new Select(dropListBox);
+        selList.selectByVisibleText("Audi");
+
+        selList.deselectAll();   // to deselect all values
+        selList.selectByIndex(0); // to select a particular value
+
+        //to get values listed in list box
+        String selValue = dropListBox.getText();
+     //   System.out.println(selValue);
+        // Alternatively create a list of webelements
+        List<WebElement> listCombo = selList.getOptions();
+        for (WebElement list:listCombo){
+            System.out.println(list.getText());
+        }
+    }
+
+    //***********************************************************************************
+    @Test
+    public void checkBox(){
+        WebElement selChkBox = driver.findElement(By.name("vehicle1"));
+        selChkBox.click();
+    }
+//******************************************************************
+    @Test
+    public void CalendarControl(){
+        WebElement dateControl = driver.findElement(By.name("bday"));
+        dateControl.sendKeys("10"+ "15" +"1982");
+    // in case TAB moves cursor then use Keys.TAB
+    //  dateControl.sendKeys("10"+ Keys.TAB +"15" +"1982");
+    }
+
+    //*************************************************************************
+
+    @Test
+    public void buttonandalert() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"home-5\"]/button")).click();
+
+        Alert alertMsg = driver.switchTo().alert();
+        Assert.assertEquals(alertMsg.getText(),"QA GURU!","Incorrect message");
+        alertMsg.accept();
+    }
+
+    // *****************************************************************************************
+    //     SYNCHRONISATION
+    // *****************************************************************************
+    // IMPLICIT
+    @ Test
+    public void implicitWait() throws InterruptedException {
+        synchronized (driver){
+            driver.wait(10000);
+        }
+    }
+
+    //    EXPLICIT WAIT
+
+    @Test
+    public void explicitWait(){
+        WebDriverWait CalWait = new WebDriverWait(driver,10);
+        WebElement CalControl = CalWait.until(ExpectedConditions.visibilityOfElementLocated(By.name("bday")));
+        //Alternatively using visibilityOf
+       // WebElement CalControl = CalWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("bday") ) ));
+        CalControl.sendKeys("10"+ "25" +"1982");
+    }
+
 }
